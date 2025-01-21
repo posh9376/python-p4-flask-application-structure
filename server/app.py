d5b56b747d9cfa7ed33d6e6ea21251f7e5b424ea
Eric Richard <reric0802@gmail.com>
2025-01-21 13:19:03 +0300

    added code to create an app

--- server/app.py
+++ server/app.py
@@ -2,4 +2,15 @@
 
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__)#instance of flask class
+
+@app.route('/')#views
+def index():
+    return "<h1>Welcome to my page</h1>"
+
+@app.route('/<string:username>')#views
+def user(username):
+    return f"<h1>Profile for {username}</h1>"
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
